4912776d4b4a2e842ab7607780bd776e2a74dddb
williecostello <williecostello@gmail.com>
2020-10-12 12:17:33 -0400

    Update book links

--- scraper_settings.py
+++ scraper_settings.py
@@ -3,8 +3,8 @@ chrome_path = '/Library/chromedriver'
 
 # Set list of book URLs to be scraped
 # (All URLs should begin with 'https://www.goodreads.com/book/show/')
-book_urls = ['https://www.goodreads.com/book/show/223380',
-             'https://www.goodreads.com/book/show/41160292']
+book_urls = ['https://www.goodreads.com/book/show/51791252-the-vanishing-half',
+             'https://www.goodreads.com/book/show/28815371-the-mothers']
 
 # Set output directory (include final forward slash!)
 output_dir = 'data/'
